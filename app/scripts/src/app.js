2bc634bb39cd0d7da325c792ecef69549389e466
Maria <mariagiraldo4@gmail.com>
2015-03-27 06:47:56 -0500

    Creating directive for informacion background-image box

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -20,6 +20,8 @@ app.config(function($routeProvider){
 app.controller('t_indexCtrl', function($scope){
 	console.log('t_indexCtrl'); 
 
+
+
     $scope.text = "hola mundo cruel!!!";
 });
 
@@ -42,25 +44,80 @@ app.directive('access', function() {
     restrict: "E",
     transclude:true,
     scope: {
-        data1: "="
+        data: "="
     },
     templateUrl: 'views/ui-components/access.html'
   };
 });
 
-//Directive for information box
+//Directive for information box component
 app.directive('boxInformation', function() {
   return {
     restrict: "E",
     transclude:true,
     scope: {
-        data1: "="
+        data: "="
     },
     templateUrl: 'views/ui-components/boxInformation.html'
   };
 });
 
+// Directive for information backgroung-image box 
+app.directive('boxInformationBgi', function() {
+  return {
+    restrict: "C",
+    transclude:true,
+    scope: {
+        data: "="
+    },
+    templateUrl: 'views/ui-components/boxInformationBgi.html'
+  };
+});
 
+
+
+//Directive for plane slider component
+app.directive('sliderPlane', function() {
+  return {
+    restrict: "E",
+    transclude:true,
+    scope: {
+        title: '@',
+        description: '@',
+        options: "="
+    },
+    link: function(scope, element, attrs){
+        console.log("link slide plane ");
+
+        scope.data = {
+        articles: [
+            { 
+                "title1": "nombre articulo" , 
+                "title2": "tedakl{sdnma{fls" , 
+                "img": "resources/img/article1.jpg"
+            }, 
+            { 
+                "title1": "nombre articulo" , 
+                "title2": "tedakl{sdnma{fls" , 
+                "img": "resources/img/article2.jpg"
+            },
+             { 
+                "title1": "nombre articulo" , 
+                "title2": "tedakl{sdnma{fls" , 
+                "img": "resources/img/article3.jpg"
+            }, 
+            { 
+                "title1": "nombre articulo" , 
+                "title2": "tedakl{sdnma{fls" , 
+                "img": "resources/img/article4.jpg"
+            }
+        ]
+    }
+        console.log(scope);
+    },
+    templateUrl: 'views/ui-components/sliderPlane.html'
+  };
+});
 
 
 
@@ -72,3 +129,4 @@ function changeColor(element){
     $(obj).css("background-color",color);
     console.log("change "+color +" element "+obj);
 };
+
